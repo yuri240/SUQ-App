e988e2e857ce662b175c470b644f4b9a3a43af04
Kitagawa <140226k@gmail.com>
2024-04-05 22:32:46 +0900

    レイアウトの整形

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :devise_controller?
   before_action :configure_permitted_parameters, if: :devise_controller?
   
   def reset_all
@@ -15,6 +15,7 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters 
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 
   def basic_auth
